0530452c9bc93d33838654ff5a6049fec2d634ff
DESKTOP-EQ55Q8H\Admin <Abikathir>
2022-05-06 21:34:31 +0530

    first commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,34 @@
+
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { UserComponent } from './user/user.component';
+import { SignUpComponent } from './user/sign-up/sign-up.component';
+import { appRoutes } from './routes';
+import { HttpClientModule } from '@angular/common/http';
+import { UserProfileComponent } from './user-profile/user-profile.component';
+import { SignInComponent } from './user/sign-in/sign-in.component';
+import { UserService } from './shared/user.service';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    UserComponent,
+    SignUpComponent,
+    UserProfileComponent,
+    SignInComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+    RouterModule.forRoot(appRoutes),
+    HttpClientModule
+  ],
+  providers: [UserService],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/shared/user.model.ts
@@ -0,0 +1,6 @@
+export class User {
+    fullName: string | undefined;
+    reg:string|undefined;
+    email: string | undefined; 
+    password: string | undefined;
+}
